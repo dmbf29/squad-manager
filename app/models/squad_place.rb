377612ce95308d224331b2cfc54e2c1a5e805e6a
Douglas Berkley <douglasmberkley@gmail.com>
2019-02-25 16:19:02 +0900

    squads controller

--- app/models/squad_place.rb
+++ app/models/squad_place.rb
@@ -3,7 +3,6 @@ class SquadPlace < ApplicationRecord
   has_many :players
 
   def self.autocreate(position_names, squad)
-    binding.pry
     position_names.each_with_index do |position_name, index|
       SquadPlace.create(name: position_name, number: index + 1, squad: squad)
     end
