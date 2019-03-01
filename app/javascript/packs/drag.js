1bf856cf32ef78ad41c6a1e1d29b38f779935d8b
Douglas Berkley <douglasmberkley@gmail.com>
2019-03-01 12:44:13 +0900

    moved drag into own pack

--- app/javascript/packs/drag.js
+++ app/javascript/packs/drag.js
@@ -4,6 +4,7 @@ window.playerDragStartHandler = (event) => {
  console.log("dragStart");
  // Add the target element's id to the data transfer object
  event.dataTransfer.setData("text/plain", event.target.id);
+ console.log(event.dataTransfer);
 }
 
 console.log('after define')
