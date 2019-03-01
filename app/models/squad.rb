780b33056f890edbd3d7cd93b9d3e22e94cb2516
Douglas Berkley <douglasmberkley@gmail.com>
2019-03-01 09:47:47 +0900

    created after_create logic for squad_rows

--- app/models/squad.rb
+++ app/models/squad.rb
@@ -16,17 +16,15 @@ class Squad < ApplicationRecord
   belongs_to :formation
   has_many :squad_places
   has_many :players, through: :squad_places
-  after_create :build_squad_places
+  after_create :build_squad_rows
 
-  def build_squad_places
-    return if squad_places.count >= 16
-    positions = ["GK"]
-    formation.dfs.times { positions << "DF"}
-    formation.mfs.times { positions << "MF"}
-    formation.fws.times { positions << "FW"}
-    positions << "On Loan"
-    positions << "Left Out"
-
-    SquadPlace.autocreate(positions, self)
+  def build_squad_rows
+    return if squad_rows.count >= 4
+    (1..4).to_a.each do |number|
+      SquadRow.create(
+        number: number,
+        squad: self
+      )
+    end
   end
 end
--- app/models/squad_row.rb
+++ app/models/squad_row.rb
@@ -11,4 +11,49 @@
 
 class SquadRow < ApplicationRecord
   belongs_to :squad
+  has_many :squad_places
+  after_create :build_squad_places
+
+  def build_squad_places
+    case number
+    when 4
+      SquadPlace.create(
+        name: "GK",
+        number: 1,
+        squad_row: self
+      )
+      SquadPlace.create(
+        name: "On Loan",
+        number: 2,
+        squad_row: self
+      )
+      SquadPlace.create(
+        name: "Other",
+        number: 3,
+        squad_row: self
+      )
+    when 3
+      (1..squad.formation.dfs.count).to_a.each do |number|
+        SquadPlace.create(
+          name: "DF",
+          number: number,
+          squad_row: self
+        )
+      end
+    when 2
+      (1..squad.formation.mfs.count).to_a.each do |number|
+        SquadPlace.create(
+          name: "MF",
+          number: number,
+          squad_row: self
+        )
+    when 1
+      (1..squad.formation.fws.count).to_a.each do |number|
+        SquadPlace.create(
+          name: "FW",
+          number: number,
+          squad_row: self
+        )
+    end
+  end
 end
