b482e88c309e1a9e7d8329762220cadcdf80c1f1
Douglas Berkley <douglasmberkley@gmail.com>
2019-03-03 16:56:55 +0900

    dropped event firing

--- app/javascript/controllers/squadplace_controller.js
+++ app/javascript/controllers/squadplace_controller.js
@@ -3,8 +3,39 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = ['content']
 
+  playerDragHandler(event) {
+    // event.dataTransfer.dropEffect = "copy";
+    // event.preventDefault();
+    // console.log("I'm dragging!");
+    // event.dataTransfer.setData("text/plain", event.target.id);
+    // event.dropEffect = "copy";
+    // event.target.style.opacity = .5;
+    event.dataTransfer.setData("text/plain", event.target.id);
+    event.dropEffect = "move";
+  }
 
-  playerDrag() {
-    console.log("I'm in the controller!");
+  playerDragOverHandler(event) {
+    // event.preventDefault();
+    // event.dataTransfer.dropEffect = 'copy';
+    // console.log("I'm dragging over!");
+    event.preventDefault();
+    // Set the dropEffect to move
+    event.dataTransfer.dropEffect = "move"
+  }
+
+  playerDropHandler(event) {
+    // event.preventDefault();
+    // console.log("I dropped!");
+    // // console.log(event.target);
+    // const data = event.dataTransfer.getData("text/plain");
+    // // console.log(event.target.querySelector('form'));
+    // console.log(data);
+    event.preventDefault();
+    console.log(event.currentTarget);
+    // Get the id of the target and add the moved element to the target's DOM
+    const data = event.dataTransfer.getData("text/plain");
+    const player = document.getElementById(data);
+    event.target.appendChild(player);
+    console.log(player.querySelector('form'))
   }
 }
--- app/javascript/packs/drag.js
+++ app/javascript/packs/drag.js
@@ -5,13 +5,13 @@ const application = Application.start()
 const context = require.context("../controllers", true, /.js$/)
 application.load(definitionsFromContext(context))
 
-console.log("loaded")
+// console.log("loaded")
 
-window.playerDragStartHandler = (event) => {
- console.log("dragStart");
- // Add the target element's id to the data transfer object
- event.dataTransfer.setData("text/plain", event.target.id);
- console.log(event.dataTransfer);
-}
+// window.playerDragStartHandler = (event) => {
+//  console.log("dragStart");
+//  // Add the target element's id to the data transfer object
+//  event.dataTransfer.setData("text/plain", event.target.id);
+//  console.log(event.dataTransfer);
+// }
 
-console.log('after define')
+// console.log('after define')
