96a6a54e270440d76debbeea76aad01f305f3c98
Douglas Berkley <douglasmberkley@gmail.com>
2019-03-03 20:33:56 +0900

    squad header

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,8 +1,24 @@
 class PlayersController < ApplicationController
 
+  def new
+    @squad_place = SquadPlace.find_by(params[:squad_place])
+    @player = Player.new
+  end
+
+  def create
+    @squad_place = SquadPlace.find_by(params[:squad_place])
+    @player = Player.new(player_params)
+    @player.squad_place = @squad_place
+    if @player.save
+      redirect_to squad_path(@player.squad_place.squad_row.squad)
+    else
+      render :new
+    end
+  end
+
   def update
     @player = Player.find(params[:id])
-    @player.update(squad_place_params)
+    @player.update(player_params)
     respond_to do |format|
       format.html { redirect_to squad_path(@player.squad_place.squad_row.squad) }
       format.js
@@ -11,7 +27,7 @@ class PlayersController < ApplicationController
 
   private
 
-  def squad_place_params
-    params.require(:player).permit(:squad_place_id)
+  def player_params
+    params.require(:player).permit(:first_name, :last_name, :nation, :age, :potential_low, :potential_high, :rating, :badge, :status, :squad_place_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'squads#index'
   resources :squads
-  resources :players, except: :index
-  resources :squad_places, only: [:edit, :update]
+  resources :players, only: [:edit, :update, :delete]
+  resources :squad_places, only: [:edit, :update] do
+    resources :players, only: [:new, :create]
+  end
 end
