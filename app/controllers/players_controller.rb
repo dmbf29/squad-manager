9d3eb2a252fabf81979312ae7e5c6a8b8f518848
Douglas Berkley <douglasmberkley@gmail.com>
2019-03-03 17:08:20 +0900

    player update js

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,6 +2,14 @@ class PlayersController < ApplicationController
 
   def update
     @player = Player.find(params[:id])
+    if @player.update(squad_place_params)
+      respond_to do |format|
+        # format.html { render 'restaurants/show' }
+        format.js
+      end
+    else
+
+    end
   end
 
   private
