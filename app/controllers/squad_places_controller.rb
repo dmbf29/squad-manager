e49cde304257b8d97ad1900ffee74e44fe7cc33f
Douglas Berkley <douglasmberkley@gmail.com>
2019-03-04 11:17:48 +0900

    controller for squad place edit

--- app/controllers/squad_places_controller.rb
+++ app/controllers/squad_places_controller.rb
@@ -1,4 +1,21 @@
 class SquadPlacesController < ApplicationController
+
   def edit
+    @squad_place = SquadPlace.find(params[:squad_place])
+  end
+
+  def update
+    @squad_place = SquadPlace.find(params[:squad_place])
+    if @squad_place.update(squad_place_params)
+      redirect_to squad_path(@squad_place.squad_row.squad)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def squad_place_params
+    params.require(:squad_place).permit(:name, :squad_row_id)
   end
 end
