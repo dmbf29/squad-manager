78464845532b8b6db147675d20819174502508ef
Douglas Berkley <douglasmberkley@gmail.com>
2019-03-05 13:40:35 +0900

    squad row controller actions

--- app/controllers/squad_rows_controller.rb
+++ app/controllers/squad_rows_controller.rb
@@ -1,2 +1,39 @@
 class SquadRowsController < ApplicationController
+
+  def new
+    @squad = Squad.find_by(params[:squad_id])
+    @squad_row = SquadRow.new
+  end
+
+  def create
+    @squad = Squad.find_by(params[:squad_id])
+    @squad_row = SquadRow.new(player_params)
+    @squad_row.squad = @squad
+    if @squad_row.save
+      redirect_to squad_path(@squad)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @squad = Squad.find_by(params[:squad_id])
+    @squad_row = SquadRow.find(params[:id])
+  end
+
+  def update
+    @squad = Squad.find_by(params[:squad])
+    @squad_row = Player.find(params[:id])
+    if @squad_row.update(player_params)
+      redirect_to squad_path(@squad)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(:number)
+  end
 end
