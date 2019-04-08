c8cab0b887444898a5349f3d8d6004f43b3aaa8a
Douglas Berkley <douglasmberkley@gmail.com>
2019-04-08 17:47:17 +0900

    Added destroy route for players

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :players, only: [:new, :create, :edit, :update]
     resources :squad_rows, only: [:new, :create, :edit, :update]
   end
-  resources :players, only: [:delete]
+  resources :players, only: [:destroy]
   resources :squad_places, only: [:edit, :update, :destroy] do
   end
 end
