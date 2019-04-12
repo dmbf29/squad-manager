90c49de75bb9a3ba3862b15442200a0f796d76ac
Douglas Berkley <douglasmberkley@gmail.com>
2019-04-12 11:53:04 +0900

    added counter for players

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# heroku run rails db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+
 # formations = %w(3142 3412 3421 343 352 41212 4132 4141 4222 4231 424 4312 4321 433 433(2) 433(3) 433(4) 433(5) 4411 4411(2) 442 442(2) 451 451(2) 5212 5221 532 541)
 formations = ["three-four-three", "three-five-two", "four-five-one", "five-four-one", "five-three-two", "four-four-two", "four-three-three"]
 numbers = ["3-4-3", "3-5-2", "4-5-1", "5-4-1", "5-3-2", "4-4-2", "4-3-3"]
