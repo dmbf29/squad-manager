12f87533d6dbf8776cc0549cda19ab0e6ffc2148
Douglas Berkley <douglasmberkley@gmail.com>
2019-11-25 17:00:55 +0900

    removed console log

--- app/javascript/controllers/squadplace_controller.js
+++ app/javascript/controllers/squadplace_controller.js
@@ -4,12 +4,8 @@ export default class extends Controller {
   static targets = ['content']
 
   playerDragHandler(event) {
-    // event.dataTransfer.dropEffect = "copy";
-    // event.preventDefault();
     // console.log("I'm dragging!");
-    // event.dataTransfer.setData("text/plain", event.target.id);
-    // event.dropEffect = "copy";
-    // event.target.style.opacity = .5;
+    // event.preventDefault();
     event.dataTransfer.setData("text/plain", event.target.id);
     event.dropEffect = "move";
   }
@@ -20,25 +16,17 @@ export default class extends Controller {
   }
 
   playerDragOverHandler(event) {
-    // event.preventDefault();
+    // console.log("I'm dragging over!");
     event.currentTarget.classList.add('gray-background');
     event.currentTarget.style = "transform:scale(1.05)";
-    // event.dataTransfer.dropEffect = 'copy';
-    // console.log("I'm dragging over!");
     event.preventDefault();
     // Set the dropEffect to move
     event.dataTransfer.dropEffect = "move"
   }
 
   playerDropHandler(event) {
-    // event.preventDefault();
     // console.log("I dropped!");
-    // // console.log(event.target);
-    // const data = event.dataTransfer.getData("text/plain");
-    // // console.log(event.target.querySelector('form'));
-    // console.log(data);
     event.preventDefault();
-    // console.log(event.currentTarget);
     // Get the id of the target and add the moved element to the target's DOM
     const data = event.dataTransfer.getData("text/plain");
     const player = document.getElementById(data);
@@ -46,10 +34,6 @@ export default class extends Controller {
     const playerForm = player.querySelector('form');
     event.currentTarget.appendChild(player);
     // TODO: Replace ID in form with new squad_place
-    // console.log("target")
-    // console.log(event.target.dataset.id)
-    // console.log("currentTarget")
-    // console.log(event.currentTarget.dataset.id)
     playerForm.querySelector('#player_squad_place_id').value = event.currentTarget.dataset.id;
     playerForm.submit(function(event) { return false });
   }
